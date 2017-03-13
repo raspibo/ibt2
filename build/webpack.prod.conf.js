4269b22a607afa801d7662fa6e487f18a2b0f690
Davide Alberani <da@erlug.linux.it>
2017-03-13 21:37:35 +0100

    #34: inject jQuery also on production

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -29,6 +29,12 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': env
     }),
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jquery: 'jquery',
+        'window.jQuery': 'jquery',
+        jQuery: 'jquery'
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
